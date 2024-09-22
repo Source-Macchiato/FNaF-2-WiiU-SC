619578dba583d1a8a0945367d02d3782fcb8ef24
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2024-09-22 20:54:33 +0200

    LevelLoader and NextNight added to NextNight.unity

---
+++ Assets/Scripts/LeverLoader/LevelLoader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    public bool requireVerification = true;
+    public Canvas canvasTV;
+    public Canvas canvasGamepad;
+
+    public GameObject loadingScreenTV;
+    public GameObject loadingScreenGamepad;
+
+    public Slider sliderTV;
+    public Slider sliderGamepad;
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operationLoadLevel = SceneManager.LoadSceneAsync(sceneName);
+
+        if (requireVerification)
+        {
+            if (canvasTV.isActiveAndEnabled)
+            {
+                loadingScreenTV.SetActive(true);
+            }
+            else
+            {
+                loadingScreenGamepad.SetActive(true);
+            }
+        }
+        else
+        {
+            sliderGamepad.gameObject.SetActive(true);
+        }
+
+        while (!operationLoadLevel.isDone)
+        {
+            float progress = Mathf.Clamp01(operationLoadLevel.progress / 0.9f);
+
+            if (requireVerification)
+            {
+                if (canvasTV.isActiveAndEnabled)
+                {
+                    sliderTV.value = progress;
+                }
+                else
+                {
+                    sliderGamepad.value = progress;
+                }
+            }
+            else
+            {
+                sliderGamepad.value = progress;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/LeverLoader/NextNight.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextNight : MonoBehaviour {
+
+    public GameObject LoadingSprite;
+
+    public GameObject Night1;
+    public GameObject Night2;
+    public GameObject Night3;
+    public GameObject Night4;
+    public GameObject Night5;
+    public GameObject Night6;
+    private float NightNumber;
+    public Text NightNumberDisplayer;
+    public GameObject loadingScreenPanel;
+
+    void Start () {
+        loadingScreenPanel.SetActive(false);
+        NightNumber = PlayerPrefs.GetFloat("NightNumber", 1);
+        LoadingSprite.SetActive(false);
+        Night1.SetActive(false);
+        Night2.SetActive(false);
+        Night3.SetActive(false);
+        Night4.SetActive(false);
+        Night5.SetActive(false);
+        Night6.SetActive(false);
+    if (NightNumber == 1) {
+        Night1.SetActive(true);
+    } else if (NightNumber == 2) {
+        Night2.SetActive(true);
+    } else if (NightNumber == 3) {
+        Night3.SetActive(true);
+    } else if (NightNumber == 4) {
+        Night4.SetActive(true);
+    } else if (NightNumber == 5) {
+        Night5.SetActive(true);
+    } else if (NightNumber == 6) {
+        Night6.SetActive(true);
+    }
+        StartCoroutine(InitCoroutine());
+    }
+
+    IEnumerator InitCoroutine() {
+        yield return new WaitForSeconds(5);
+        loadingScreenPanel.SetActive(true);
+        LoadingSprite.SetActive(true);
+        GameObject.Find("LevelLoader").GetComponent<LevelLoader>().LoadLevel("Office");
+    }
+}
