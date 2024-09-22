7089368a52672b7fc241022a1056908a54ac1dc1
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2024-09-22 17:52:14 +0200

    video player with new intro added

---
+++ Assets/Scripts/UI/RenderMovie.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RenderMovie : MonoBehaviour
+{
+    public AudioSource SrcMacchiatoSound;
+    public MovieTexture movTexture;
+    bool hasStarted = false;
+
+    void Start()
+    {
+        movTexture.Play();
+        SrcMacchiatoSound.Play();
+    }
+
+    void Update()
+    {
+        if (!hasStarted && movTexture.isPlaying)
+        {
+            hasStarted = true;
+
+        }
+
+        if (hasStarted && !movTexture.isPlaying)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
