4b678c024050f9db977c349fe7353354cfa6b2ce
Xyla-TV <namilabest@gmail.com>
2024-11-27 17:25:50 +0100

    asset bundle for vo language pack

---
+++ Assets/Scripts/Editor/ExportAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ExportAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundle")]
+    static void ExportResource()
+    {
+        string folderName = "AssetBundles";
+        string filePath = Path.Combine(Application.streamingAssetsPath, folderName);
+
+        // Build for Wii U
+        BuildPipeline.BuildAssetBundles(filePath, BuildAssetBundleOptions.None, BuildTarget.WiiU);
+
+        // Refresh the Project folder
+        AssetDatabase.Refresh();
+    }
+}
