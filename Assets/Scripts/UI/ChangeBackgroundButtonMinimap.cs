665daab0bf562adcc9125d7fe4d7ead649c3c9c9
Xyla-TV <namilabest@gmail.com>
2024-11-27 11:17:39 +0100

    animation when a button is selected

---
+++ Assets/Scripts/UI/ChangeBackgroundButtonMinimap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ChangeBackgroundButtonMinimap : MonoBehaviour
+{
+    public GameObject[] camerasBackground;
+    public GameObject minimapPanel;
+    private string currentCameraName;
+
+    void Update()
+    {
+        if (currentCameraName != null)
+        {
+            foreach (GameObject background in camerasBackground)
+            {
+                Animator animator = background.GetComponent<Animator>();
+
+                if (background.name == (currentCameraName + "-Background"))
+                {
+                    animator.Play("Blink");
+                }
+                else
+                {
+                    animator.Play("Idle");
+                }
+            }
+        }
+    }
+
+    public void ButtonPressed(string cameraName)
+    {
+        if (currentCameraName != cameraName)
+        {
+            currentCameraName = cameraName;
+        }
+    }
+}
