72d2568fce562f340a8e8b8ddc04aff76eec5cb4
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-12-13 10:51:42 +0100

    better system for display current night in NextNight

--- Assets/Scripts/LeverLoader/NextNight.cs
+++ Assets/Scripts/LeverLoader/NextNight.cs
@@ -1,51 +1,42 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class NextNight : MonoBehaviour {
+public class NextNight : MonoBehaviour
+{
+    private int nightNumber;
 
-    public GameObject LoadingSprite;
+    public GameObject[] nightDisplayers;
+    public GameObject loadingScreen;
 
-    public GameObject Night1;
-    public GameObject Night2;
-    public GameObject Night3;
-    public GameObject Night4;
-    public GameObject Night5;
-    public GameObject Night6;
-    private float NightNumber;
-    public Text NightNumberDisplayer;
-    public GameObject loadingScreenPanel;
+    LevelLoader levelLoader;
 
-    void Start () {
-        loadingScreenPanel.SetActive(false);
-        NightNumber = PlayerPrefs.GetFloat("NightNumber", 1);
-        LoadingSprite.SetActive(false);
-        Night1.SetActive(false);
-        Night2.SetActive(false);
-        Night3.SetActive(false);
-        Night4.SetActive(false);
-        Night5.SetActive(false);
-        Night6.SetActive(false);
-    if (NightNumber == 1) {
-        Night1.SetActive(true);
-    } else if (NightNumber == 2) {
-        Night2.SetActive(true);
-    } else if (NightNumber == 3) {
-        Night3.SetActive(true);
-    } else if (NightNumber == 4) {
-        Night4.SetActive(true);
-    } else if (NightNumber == 5) {
-        Night5.SetActive(true);
-    } else if (NightNumber == 6) {
-        Night6.SetActive(true);
-    }
+    void Start()
+    {
+        // Get LevelLoader script
+        levelLoader = FindObjectOfType<LevelLoader>();
+
+        // Disable loading screen when the game starts
+        loadingScreen.SetActive(false);
+
+        // Get night number
+        nightNumber = SaveManager.LoadNightNumber();
+
+        // Display current night displayer and hide others
+        for (int i = 0; i < nightDisplayers.Length; i++)
+        {
+            nightDisplayers[i].SetActive(i == nightNumber);
+        }
+
+        // The timer before load the next scene
         StartCoroutine(InitCoroutine());
     }
 
-    IEnumerator InitCoroutine() {
+    IEnumerator InitCoroutine()
+    {
         yield return new WaitForSeconds(5);
-        loadingScreenPanel.SetActive(true);
-        LoadingSprite.SetActive(true);
-        GameObject.Find("LevelLoader").GetComponent<LevelLoader>().LoadLevel("Office");
+
+        loadingScreen.SetActive(true);
+
+        levelLoader.LoadLevel("Office");
     }
 }
