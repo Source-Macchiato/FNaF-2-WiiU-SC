b6ff1e81f801be949264d3749033a3dd4cc6497c
Xyla-TV <namilabest@gmail.com>
2024-12-18 12:43:11 +0100

    TheEnd scene and load MainMenu after 20 seconds

---
+++ Assets/Scripts/LevelLoader/TheEnd.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+	private int nightNumber;
+
+	// Scripts
+	LevelLoader levelLoader;
+
+	void Start()
+	{
+		// Get scripts
+		levelLoader = FindObjectOfType<LevelLoader>();
+
+		// Disable loading screen when the game starts
+		levelLoader.loadingScreen.SetActive(false);
+
+		// Get night number
+		nightNumber = SaveManager.LoadNightNumber();
+
+		StartCoroutine(LoadNextScene());
+	}
+	
+	private IEnumerator LoadNextScene()
+	{
+		yield return new WaitForSeconds(20f);
+
+		// Enable loading screen
+		levelLoader.loadingScreen.SetActive(true);
+
+		// Load next scene
+		levelLoader.LoadLevel("MainMenu");
+	}
+}
