23b67a434bf5f2087827beac6404e5d085d660c7
Xyla-TV <namilabest@gmail.com>
2024-12-24 11:38:36 +0100

    functions for monitor

--- Assets/Scripts/GameScript/MonitorManager.cs
+++ Assets/Scripts/GameScript/MonitorManager.cs
@@ -3,15 +3,25 @@ using WiiU = UnityEngine.WiiU;
 
 public class MonitorManager : MonoBehaviour
 {
+    public bool isMonitorActive = false;
+
     // References to WiiU controllers
     WiiU.GamePad gamePad;
     WiiU.Remote remote;
+
+    // Scripts
+    NightPlayer nightPlayer;
+    MaskManager maskManager;
 
     void Start()
 	{
         // Access the WiiU GamePad and Remote
         gamePad = WiiU.GamePad.access;
         remote = WiiU.Remote.Access(0);
+
+        // Get scripts
+        nightPlayer = FindObjectOfType<NightPlayer>();
+        maskManager = FindObjectOfType<MaskManager>();
     }
 	
 	void Update()
@@ -24,7 +34,7 @@ public class MonitorManager : MonoBehaviour
         {
             if (gamePadState.IsTriggered(WiiU.GamePadButton.L))
             {
-
+                ToggleMonitor();
             }
         }
 
@@ -33,19 +43,19 @@ public class MonitorManager : MonoBehaviour
             case WiiU.RemoteDevType.ProController:
                 if (remoteState.pro.IsTriggered(WiiU.ProControllerButton.L))
                 {
-
+                    ToggleMonitor();
                 }
                 break;
             case WiiU.RemoteDevType.Classic:
                 if (remoteState.classic.IsTriggered(WiiU.ClassicButton.L))
                 {
-
+                    ToggleMonitor();
                 }
                 break;
             default:
                 if (remoteState.IsTriggered(WiiU.RemoteButton.One))
                 {
-
+                    ToggleMonitor();
                 }
                 break;
         }
@@ -54,8 +64,41 @@ public class MonitorManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.L))
             {
-
+                ToggleMonitor();
             }
         }
+
+        if (nightPlayer.isJumpscared && isMonitorActive)
+        {
+            DisableMonitor();
+        }
+    }
+
+    private void ToggleMonitor()
+    {
+        if (!nightPlayer.isJumpscared)
+        {
+            if (isMonitorActive)
+            {
+                DisableMonitor();
+            }
+            else
+            {
+                if (!maskManager.isMaskActive)
+                {
+                    EnableMonitor();
+                }
+            }
+        }
+    }
+
+    private void EnableMonitor()
+    {
+
+    }
+
+    private void DisableMonitor()
+    {
+
     }
 }
