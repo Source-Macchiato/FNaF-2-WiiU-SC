86870c3fd946d18d16200c28f22769f38d140df5
Xyla-TV <namilabest@gmail.com>
2024-12-26 15:55:06 +0100

    system for hide or display stars

---
+++ Assets/Scripts/UI/StarsDisplayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class StarsDisplayer : MonoBehaviour
+{
+	public GameObject[] starContainers;
+
+	void Start()
+	{
+		// Enable or disable stars based on save 
+		for (int i = 0; i < starContainers.Length; i++)
+		{
+			starContainers[i].SetActive(SaveManager.LoadUnlockedStars(i));
+		}
+	}
+}
