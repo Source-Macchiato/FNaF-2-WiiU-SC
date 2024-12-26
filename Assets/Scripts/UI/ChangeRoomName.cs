f0f5897a759b436f0c9b95477b382562b2c76c08
Xyla-TV <namilabest@gmail.com>
2024-12-26 12:49:48 +0100

    system for change room name on top of cameras

---
+++ Assets/Scripts/UI/ChangeRoomName.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ChangeRoomName : MonoBehaviour
+{
+	// Scripts
+	I18nTextTranslator i18nTextTranslator;
+
+	void Start()
+	{
+		// Get scripts
+		i18nTextTranslator = GetComponent<I18nTextTranslator>(); // Get script in current game object
+	}
+	
+	public void ChangeRoomId(string id)
+	{
+		i18nTextTranslator.textId = id;
+		i18nTextTranslator.UpdateText();
+	}
+}
