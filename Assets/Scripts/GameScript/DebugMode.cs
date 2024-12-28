4804cbd1142eb51931a25099f39dc74d934dc287
shiro-sata <125814898+shiro-sata@users.noreply.github.com>
2024-12-28 03:24:22 +0100

    Finished the debug mode

--- Assets/Scripts/GameScript/DebugMode.cs
+++ Assets/Scripts/GameScript/DebugMode.cs
@@ -4,6 +4,9 @@ public class DebugMode : MonoBehaviour {
     public NightPlayer nightPlayer;
     public GameObject DebugObject;
     public bool DebugModeActive;
+
+    //Text of the cam number of each animatronics
+    [Header("Animatronics Cams Info")]
     public Text ToyFreddy;
     public Text ToyBonnie;
     public Text ToyChica;
@@ -13,6 +16,11 @@ public class DebugMode : MonoBehaviour {
     public Text WFoxy;
     public Text Mangle;
     public Text BaloonBoy;
+
+    //FPS Counter
+    [Header("FPS Counter")]
+    public Text fpsText; 
+    private float deltaTime;
     private void Update()
     {
         if(DebugModeActive)
@@ -24,6 +32,15 @@ public class DebugMode : MonoBehaviour {
     }
     void SetDebug()
     {
+        //time elapse each frames
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        //FPS calc
+        float fps = 1.0f / deltaTime;
+
+        //display the FPS
+        fpsText.text = Mathf.Ceil(fps).ToString();
+
+        // set Animatronics Cams Info on each text
         ToyFreddy.text = nightPlayer.ToyFreddyCamera.ToString();
         ToyBonnie.text = nightPlayer.ToyBonnieCamera.ToString();
         ToyChica.text = nightPlayer.ToyChicaCamera.ToString();
@@ -33,6 +50,8 @@ public class DebugMode : MonoBehaviour {
         WFoxy.text = nightPlayer.WitheredFoxyCamera.ToString();
         Mangle.text = nightPlayer.MangleCamera.ToString();
         BaloonBoy.text = nightPlayer.BBCamera.ToString();
+
+
     }
     
 }
