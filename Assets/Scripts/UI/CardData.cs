47b2c4b1d839eee534d2022a06869295a536ef54
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-12-31 13:35:58 +0100

    better system for color animation

--- Assets/Scripts/UI/CardData.cs
+++ Assets/Scripts/UI/CardData.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
@@ -7,57 +6,15 @@ public class CardData : MonoBehaviour
 {
     public string cardId;
     public Image cursorImage;
-    private Color originalColor;
-    private Coroutine colorChangeCoroutine;
-
-    void Start()
-    {
-        // Keep original color
-        originalColor = cursorImage.color;
-    }
+    public Animator cursorAnimator;
 
     void Update()
     {
-        if (EventSystem.current.currentSelectedGameObject == gameObject)
-        {
-            // Display cursor
-            Color color = cursorImage.color;
-            color.a = 1f;
-            cursorImage.color = color;
-        }
-        else
-        {
-            // Hide cursor
-            Color color = cursorImage.color;
-            color.a = 0f;
-            cursorImage.color = color;
-
-            // Stop coroutine if it is playing and reset color
-            if (colorChangeCoroutine != null)
-            {
-                StopCoroutine(colorChangeCoroutine);
-                colorChangeCoroutine = null;
-                // Ensure the color is reset when deselected
-                cursorImage.color = originalColor;
-            }
-        }
+        cursorImage.enabled = EventSystem.current.currentSelectedGameObject == gameObject;
     }
 
     public void SelectedCardAnimation()
     {
-        colorChangeCoroutine = StartCoroutine(ChangeColorCoroutine());
-    }
-
-    private IEnumerator ChangeColorCoroutine()
-    {
-        // Change color to yellow
-        cursorImage.color = Color.yellow;
-
-        yield return new WaitForSeconds(0.2f);
-
-        if (EventSystem.current.currentSelectedGameObject == gameObject)
-        {
-            cursorImage.color = originalColor;
-        }
+        cursorAnimator.Play("Selected");
     }
 }
