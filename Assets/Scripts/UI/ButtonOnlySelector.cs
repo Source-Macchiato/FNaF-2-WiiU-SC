c856576a8d121fd38e71b4631e25d5bec2645746
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2024-12-31 12:04:54 +0100

    prevent event system to deselect if click on background

---
+++ Assets/Scripts/UI/ButtonOnlySelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonOnlySelector : MonoBehaviour
+{
+    GameObject lastSelected;
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            if (lastSelected && lastSelected.gameObject.activeSelf && lastSelected.GetComponent<Button>() != null && lastSelected.GetComponent<Button>().interactable)
+            {
+                EventSystem.current.SetSelectedGameObject(lastSelected);
+            }
+        }
+        else
+        {
+            lastSelected = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
