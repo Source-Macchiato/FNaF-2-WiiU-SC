daf592c32ca49c95bf1976f0cbe16d9f0e9caa98
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-01-01 15:34:03 +0100

    system for display current night number

---
+++ Assets/Scripts/UI/DisplayNightNumber.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using RTLTMPro;
+
+public class DisplayNightNumber : MonoBehaviour
+{
+	private int nightNumber;
+	private RTLTextMeshPro nightNumberText;
+
+	void Start()
+	{
+		nightNumber = SaveManager.LoadNightNumber();
+		nightNumberText = GetComponent<RTLTextMeshPro>();
+
+		nightNumberText.text = (nightNumber + 1).ToString();
+	}
+}
