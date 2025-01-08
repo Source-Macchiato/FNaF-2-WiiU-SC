b61c1bb0376585b74cea9ef554c588bb191fe5be
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-01-08 12:48:38 +0100

    translate selected option in custom night switcher

--- Assets/Scripts/UI/SwitcherData.cs
+++ Assets/Scripts/UI/SwitcherData.cs
@@ -8,6 +8,7 @@ public class SwitcherData : MonoBehaviour
     public string[] optionsName;
 
     public RTLTextMeshPro text;
+    public I18nTextTranslator i18nTextTranslator;
 
     void Update()
     {
@@ -16,6 +17,15 @@ public class SwitcherData : MonoBehaviour
             if (text.text != optionsName[currentOptionId] || text.text == null)
             {
                 text.text = optionsName[currentOptionId];
+            }
+        }
+
+        if (i18nTextTranslator != null)
+        {
+            if (i18nTextTranslator.textId != optionsName[currentOptionId])
+            {
+                i18nTextTranslator.textId = optionsName[currentOptionId];
+                i18nTextTranslator.UpdateText();
             }
         }
     }
