8882450dd8df7ae1e9977c997b19dcd761662dd5
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-01-31 14:30:43 +0100

    system for set mode finished in 6AM

--- Assets/Scripts/LevelLoader/SixAM.cs
+++ Assets/Scripts/LevelLoader/SixAM.cs
@@ -8,12 +8,24 @@ public class SixAM : MonoBehaviour
 
 	private int nightNumber;
 
-	// Scripts
-	SaveGameState saveGameState;
-	SaveManager saveManager;
-    LevelLoader levelLoader;
+	// AI levels
+	private int freddyAI;
+    private int bonnieAI;
+    private int chicaAI;
+	private int foxyAI;
+	private int balloonBoyAI;
+	private int toyFreddyAI;
+	private int toyBonnieAI;
+	private int toyChicaAI;
+	private int mangleAI;
+	private int goldenFreddyAI;
 
-    void Start()
+    // Scripts
+    SaveGameState saveGameState;
+	SaveManager saveManager;
+	LevelLoader levelLoader;
+
+	void Start()
 	{
 		// Get scripts
 		saveGameState = FindObjectOfType<SaveGameState>();
@@ -26,13 +38,30 @@ public class SixAM : MonoBehaviour
 		// Get current night number
 		nightNumber = SaveManager.LoadNightNumber();
 
-		// Increase night number and save it
+        // Increase night number and save it
         nightNumber++;
 
-        saveManager.SaveNightNumber(nightNumber);
-        bool saveResult = saveGameState.DoSave();
+		saveManager.SaveNightNumber(nightNumber);
+		bool saveResult = saveGameState.DoSave();
 
-        StartCoroutine(LoadNextScene());
+		if (nightNumber == 7)
+		{
+            // Get AI levels
+            freddyAI = PlayerPrefs.GetInt("FreddyAI", 0);
+            bonnieAI = PlayerPrefs.GetInt("BonnieAI", 0);
+            chicaAI = PlayerPrefs.GetInt("ChicaAI", 0);
+            foxyAI = PlayerPrefs.GetInt("FoxyAI", 0);
+            balloonBoyAI = PlayerPrefs.GetInt("BalloonBoyAI", 0);
+            toyFreddyAI = PlayerPrefs.GetInt("ToyFreddyAI", 0);
+            toyBonnieAI = PlayerPrefs.GetInt("ToyBonnieAI", 0);
+            toyChicaAI = PlayerPrefs.GetInt("ToyChicaAI", 0);
+            mangleAI = PlayerPrefs.GetInt("MangleAI", 0);
+            goldenFreddyAI = PlayerPrefs.GetInt("GoldenFreddyAI", 0);
+
+			SetModeFinished();
+        }
+
+		StartCoroutine(LoadNextScene());
 	}
 
 	private IEnumerator LoadNextScene()
@@ -63,4 +92,62 @@ public class SixAM : MonoBehaviour
 			levelLoader.LoadLevel("TheEnd");
 		}
 	}
+
+	private void SetModeFinished()
+	{
+		if (CheckCustomNightMode(20, 20, 20, 20, 0, 0, 0, 0, 0, 0))
+		{
+			saveManager.SaveDoneMode(0, true);
+		}
+		else if (CheckCustomNightMode(0, 0, 0, 0, 10, 10, 10, 10, 10, 0))
+		{
+			saveManager.SaveDoneMode(1, true);
+		}
+		else if (CheckCustomNightMode(0, 20, 0, 5, 0, 0, 20, 0, 0, 0))
+		{
+			saveManager.SaveDoneMode(2, true);
+		}
+		else if (CheckCustomNightMode(0, 0, 0, 0, 20, 0, 0, 0, 20, 10))
+		{
+			saveManager.SaveDoneMode(3, true);
+		}
+		else if (CheckCustomNightMode(0, 0, 0, 20, 0, 0, 0, 0, 20, 0))
+		{
+			saveManager.SaveDoneMode(4, true);
+		}
+		else if (CheckCustomNightMode(0, 0, 20, 0, 0, 0, 0, 20, 20, 0))
+		{
+			saveManager.SaveDoneMode(5, true);
+		}
+		else if (CheckCustomNightMode(20, 0, 0, 10, 10, 20, 0, 0, 0, 10))
+		{
+			saveManager.SaveDoneMode(6, true);
+		}
+		else if (CheckCustomNightMode(5, 5, 5, 5, 5, 5, 5, 5, 5, 5))
+		{
+			saveManager.SaveDoneMode(7, true);
+		}
+		else if (CheckCustomNightMode(10, 10, 10, 10, 10, 10, 10, 10, 10, 10))
+		{
+			saveManager.SaveDoneMode(8, true);
+		}
+		else if (CheckCustomNightMode(20, 20, 20, 20, 20, 20, 20, 20, 20, 20))
+		{
+			saveManager.SaveDoneMode(9, true);
+		}
+	}
+
+	private bool CheckCustomNightMode(int freddy, int bonnie, int chica, int foxy, int bb, int toyFreddy, int toyBonnie, int toyChica, int mangle, int goldenFreddy)
+	{
+		return freddyAI == freddy &&
+			bonnieAI == bonnie &&
+			chicaAI == chica &&
+			foxyAI == foxy &&
+			balloonBoyAI == bb &&
+			toyFreddyAI == toyFreddy &&
+			toyBonnieAI == toyBonnie &&
+			toyChicaAI == toyChica &&
+			mangleAI == mangle &&
+			goldenFreddyAI == goldenFreddy;
+    }
 }
