891a93febeb7449fe65c61f09aa4753fb88c6df8
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-02-03 16:04:26 +0100

    system for change ready button color on selected

---
+++ Assets/Scripts/UI/ButtonSelectionHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonSelectionHandler : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    private Button button;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        button.OnPointerEnter(null);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        button.OnPointerExit(null);
+    }
+}
