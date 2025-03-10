28499e91e392654e0ee3d0b8cf3efd37aa862e55
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-03-10 16:52:25 +0100

    system for display lights and pumpkin at halloween

---
+++ Assets/Scripts/HalloweenEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class HalloweenEvent : MonoBehaviour
+{
+	public GameObject lights;
+	public GameObject pumpkin;
+
+	void Start()
+	{
+		DateTime currentDate = DateTime.Now;
+
+		if (currentDate.Day == 31 && currentDate.Month == 10)
+		{
+			lights.SetActive(true);
+			pumpkin.SetActive(true);
+		}
+		else
+		{
+			lights.SetActive(false);
+			pumpkin.SetActive(false);
+		}
+	}
+}
