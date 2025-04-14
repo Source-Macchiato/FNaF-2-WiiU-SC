60c4646986555fb8dd40b0f09bb3176c57ef2b79
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-04-14 16:22:48 +0200

    loading scene when touch kids

---
+++ Assets/Scripts/MiniGames/KidEnd.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class KidEnd : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            MiniGamesLevelLoader.LoadScene("MainMenu");
+        }
+    }
+}
