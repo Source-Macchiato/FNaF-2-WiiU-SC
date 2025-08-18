88f017d17eabb4ef56b6432cd619b08b03af3688
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-18 14:21:55 +0200

    Added list of achievements and icons

---
+++ Assets/Scripts/Achievements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class Achievements
+{
+    public enum achievements
+    {
+        ONENIGHTATFREDDYS,
+        TWONIGHTSATFREDDYS,
+        THREENIGHTSATFREDDYS,
+        FOURNIGHTSATFREDDYS,
+        FIVENIGHTSATFREDDYS,
+        YOUSTAYED,
+        YOUTAMPERED,
+        YOUTRIED,
+        YOUSOOTHED,
+        YOUFAILED
+    }
+}
