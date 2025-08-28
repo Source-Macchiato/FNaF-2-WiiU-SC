477ae2e1852a5960854941a7c076c56c5720dd6c
Alyx <50267518+Xyla-TV@users.noreply.github.com>
2025-08-28 13:32:10 +0200

    Added home menu status

---
+++ Assets/Scripts/HomeMenuStatus.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using WiiU = UnityEngine.WiiU;
+
+public class HomeMenuStatus : MonoBehaviour
+{
+	public bool enableHomeMenu = false;
+
+	void Update()
+	{
+		WiiU.Core.homeMenuEnabled = enableHomeMenu && !SaveGameState.isSaving;
+	}
+}
